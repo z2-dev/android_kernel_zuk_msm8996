b2592c46caf4a3f7f84ab21d8715ce8340d22115
Cosme Domínguez Díaz <cosme.ddiaz@gmail.com>
2017-09-18 11:31:11 +0200

    Move camera_v2z to camera_v2 for make cherry-picks easier.

--- drivers/media/platform/msm/camera_v2/fd/msm_fd_dev.c
+++ drivers/media/platform/msm/camera_v2/fd/msm_fd_dev.c
@@ -340,7 +340,7 @@ static int msm_fd_vbif_error_handler(void *handle, uint32_t error)
 	int ret;
 
 	if (NULL == handle) {
-		pr_err("FD Ctx is null, Cannot recover\n");
+		dev_err(fd->dev, "FD Ctx is null, Cannot recover\n");
 		return 0;
 	}
 	ctx = (struct fd_ctx *)handle;
@@ -747,13 +747,9 @@ static int msm_fd_s_fmt_vid_out(struct file *file,
 static int msm_fd_reqbufs(struct file *file,
 	void *fh, struct v4l2_requestbuffers *req)
 {
-	int ret;
 	struct fd_ctx *ctx = msm_fd_ctx_from_fh(fh);
 
-	mutex_lock(&ctx->fd_device->recovery_lock);
-	ret = vb2_reqbufs(&ctx->vb2_q, req);
-	mutex_unlock(&ctx->fd_device->recovery_lock);
-	return ret;
+	return vb2_reqbufs(&ctx->vb2_q, req);
 }
 
 /*
@@ -765,14 +761,9 @@ static int msm_fd_reqbufs(struct file *file,
 static int msm_fd_qbuf(struct file *file, void *fh,
 	struct v4l2_buffer *pb)
 {
-	int ret;
 	struct fd_ctx *ctx = msm_fd_ctx_from_fh(fh);
 
-	mutex_lock(&ctx->fd_device->recovery_lock);
-	ret = vb2_qbuf(&ctx->vb2_q, pb);
-	mutex_unlock(&ctx->fd_device->recovery_lock);
-	return ret;
-
+	return vb2_qbuf(&ctx->vb2_q, pb);
 }
 
 /*
@@ -784,13 +775,9 @@ static int msm_fd_qbuf(struct file *file, void *fh,
 static int msm_fd_dqbuf(struct file *file,
 	void *fh, struct v4l2_buffer *pb)
 {
-	int ret;
 	struct fd_ctx *ctx = msm_fd_ctx_from_fh(fh);
 
-	mutex_lock(&ctx->fd_device->recovery_lock);
-	ret = vb2_dqbuf(&ctx->vb2_q, pb, file->f_flags & O_NONBLOCK);
-	mutex_unlock(&ctx->fd_device->recovery_lock);
-	return ret;
+	return vb2_dqbuf(&ctx->vb2_q, pb, file->f_flags & O_NONBLOCK);
 }
 
 /*
@@ -1055,18 +1042,14 @@ static int msm_fd_s_ctrl(struct file *file, void *fh, struct v4l2_control *a)
 			a->value = ctx->format.size->work_size;
 		break;
 	case V4L2_CID_FD_WORK_MEMORY_FD:
-		mutex_lock(&ctx->fd_device->recovery_lock);
 		if (ctx->work_buf.fd != -1)
 			msm_fd_hw_unmap_buffer(&ctx->work_buf);
 		if (a->value >= 0) {
 			ret = msm_fd_hw_map_buffer(&ctx->mem_pool,
 				a->value, &ctx->work_buf);
-			if (ret < 0) {
-				mutex_unlock(&ctx->fd_device->recovery_lock);
+			if (ret < 0)
 				return ret;
-			}
 		}
-		mutex_unlock(&ctx->fd_device->recovery_lock);
 		break;
 	default:
 		return -EINVAL;
