f4af7ab135ed0218b17f7af049c42a8580d48f07
Sultanxda <sultanxda@gmail.com>
2017-11-29 01:23:18 -0800

    kthread: Force all non-percpu kthreads onto the power cluster
    Force all kthreads that don't need to be on a specific CPU onto the power
    cluster in order to save power. This overrides anyone (user-land and
    kernel-land) who wants to change the CPU affinity of a non-percpu kthread.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -1697,6 +1697,9 @@ static struct worker *create_worker(struct worker_pool *pool)
 		goto fail;
 
 	set_user_nice(worker->task, pool->attrs->nice);
+
+	/* prevent anyone from meddling with cpumask of workqueue workers */
+	worker->task->flags |= PF_NO_SETAFFINITY;
 	worker->task->kthread_per_cpu = true;
 
 	/* successful, attach the worker to the pool */
